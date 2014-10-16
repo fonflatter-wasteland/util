e5cd3ad63a8b7f296ba44b0e38487f4aea5bbf80
winniehell <git@winniehell.de>
2014-10-17 01:02:04 +0200

    Use last commit hash instead of HEAD
    The symbolic ref HEAD is not always available.

--- merge.sh
+++ merge.sh
@@ -5,16 +5,18 @@ set -e
 : ${OUTPUT_DIR:?}
 
 INPUT_REPO=`git config --get remote.github.url | sed -r 's,^(https://github\.com/|git@github\.com:),,' | sed -r 's,\.git$,,'`
-INPUT_BRANCH=`git symbolic-ref HEAD | sed 's,^refs/heads/,,'`
+COMMIT_HASH=`git log -1 --pretty='%H'`
+COMMIT_MESSAGE=`git log -1 --pretty='%B'`
 
 # transfer content from input to output
-git remote add tmp $OUTPUT_DIR
-git push tmp $INPUT_BRANCH:input
-git remote rm tmp
+git remote add output $OUTPUT_DIR
+git branch input $COMMIT_HASH
+git push output input
+git branch -d input
+git remote rm output
 
 # merge content and upload output
 cd $OUTPUT_DIR
-MESSAGE=`git log -1 --pretty='%B' input`
-git merge --no-ff -m "$INPUT_REPO: $MESSAGE" input
+git merge --no-ff -m "$INPUT_REPO: $COMMIT_MESSAGE" input
 git branch -d input
 git push
