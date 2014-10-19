7b4b96303b232f2834a9451f4a647259bacd8515
winniehell <git@winniehell.de>
2014-10-19 21:17:04 +0200

    Add publish script
    script which commits and uploads the content of a Jekyll output folder

---
+++ publish.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+
+: ${TARGET_REPO_BRANCH:?}
+: ${TARGET_REPO_DIR:?}
+: ${TRAVIS_BUILD_DIR:?}
+
+cd "${TARGET_REPO_DIR}"
+
+echo "Overwriting content of ${TARGET_REPO_BRANCH}..."
+rsync --archive --delete "${TRAVIS_BUILD_DIR}/_site/" "${TARGET_REPO_DIR}/"
+git add --all .
+git commit -m "Automatic build by Travis CI"
+git push
