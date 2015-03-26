4f5854ab9e7321c0409a8cfab519078b40eafaa5
winniehell <git@winniehell.de>
2015-03-26 21:59:16 +0100

    Script for synchronizing Git repositories

---
+++ sync.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash                                                                                                                                                                      
+
+# This script synchronizes all Git repositories in subdirectories with upstream                                                                                                          
+
+set -o nounset
+set -o errexit
+
+BASE_PATH=${PWD}
+
+find ${BASE_PATH} -name '.git' | sed 's|\.git$||' | while read REPOSITORY_PATH
+do
+    echo "Synchronizing ${REPOSITORY_PATH/${BASE_PATH}\//} ..."
+
+    cd ${REPOSITORY_PATH}
+    git add --all
+
+    # only commit if there is anything to commit                                                                                                                                         
+    git diff-index --quiet --cached HEAD || git commit -m 'Add new content'
+
+    git pull --rebase
+    git push
+
+    echo "Done." && echo
+done
+
+exit 0
